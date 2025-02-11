552a7d772858ccd647d4fcbee7e14772eac172d2
LJ-Fryer <ljf_aus@hotmail.com>
2025-02-11 13:27:09 +1100

    Added footer to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Testing</div>;
+  return (
+    <div className="App">
+      <h1>Testing</h1>
+      <footer>
+        Project coded by{" "}
+        <a
+          href="https://lj-fryer.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LJ Fryer
+        </a>
+        . Open-sourced on{" "}
+        <a
+          href="https://github.com/LJ-Fryer/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
 
 export default App;
