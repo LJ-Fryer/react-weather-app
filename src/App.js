c1b213a5403eacf72b451b273c4ff632bb92882b
LJ-Fryer <ljf_aus@hotmail.com>
2025-02-11 16:59:32 +1100

    Added bootstrap container to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing</h1>
-      <footer>
-        Project coded by{" "}
-        <a
-          href="https://lj-fryer.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          LJ Fryer
-        </a>
-        . Open-sourced on{" "}
-        <a
-          href="https://github.com/LJ-Fryer/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Testing</h1>
+        <footer>
+          Project coded by{" "}
+          <a
+            href="https://lj-fryer.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LJ Fryer
+          </a>
+          . Open-sourced on{" "}
+          <a
+            href="https://github.com/LJ-Fryer/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
