c799308f861c607615ddc48e15076c411937e845
LJ-Fryer <ljf_aus@hotmail.com>
2025-02-17 00:45:07 +1100

    Added link to Netlify in footer

--- src/App.js
+++ src/App.js
@@ -8,21 +8,29 @@ function App() {
       <div className="container ">
         <Weather defaultCity="Sydney" />
         <footer>
-          Project coded by{" "}
+          Coded by{" "}
           <a
             href="https://lj-fryer.netlify.app/"
             target="_blank"
             rel="noreferrer"
           >
             LJ Fryer
-          </a>{" "}
-          and open-sourced on{" "}
+          </a>
+          , open-sourced on{" "}
           <a
             href="https://github.com/LJ-Fryer/react-weather"
             target="_blank"
             rel="noreferrer"
           >
             GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://ljf-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
         </footer>
       </div>
