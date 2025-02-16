56a2b9166547d60ef98626619a680cf6042be5bd
LJ-Fryer <ljf_aus@hotmail.com>
2025-02-17 00:27:31 +1100

    Improved styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,49 +4,18 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1 className="city">{props.data.city}</h1>
-      <p className="condition-description text-capitalize">
+      <h1>{props.data.city}</h1>
+      <p className="mt-1 condition-description text-capitalize">
         {props.data.condition}
+        <span>💧 {props.data.humidity}%</span>{" "}
+        <span>💨 {props.data.wind}km/h</span>
       </p>
-
+      <p></p>
       <div className="d-flex temperature-box">
         <div className="temperature-inner-box">
           <img src={props.data.iconUrl} alt={props.data.iconAlt} />
         </div>
         <WeatherTemperature celsius={props.data.temperature} />
-      </div>
-
-      <div>
-        <div className="row">
-          <ul className="col-1 pt-1 pb-2 conditions-box-left">
-            <li className="conditions-icon">{""}</li>
-            <li>{""}</li>
-          </ul>
-          <ul className="col-1 pt-1 pb-2">
-            <li className="conditions-icon">☂️</li>
-            <li>
-              15<span className="conditions-unit">%</span>
-            </li>
-          </ul>
-          <ul className="col-1 pt-1 pb-2">
-            <li className="conditions-icon">💧</li>
-            <li>
-              {props.data.humidity}
-              <span className="conditions-unit">%</span>
-            </li>
-          </ul>
-          <ul className="col-1 pt-1 pb-2">
-            <li className="conditions-icon">💨</li>
-            <li>
-              {props.data.wind}
-              <span className="conditions-unit">km/h</span>
-            </li>
-          </ul>
-          <ul className="col-1 pt-1 pb-2 conditions-box-right">
-            <li className="conditions-icon">{""}</li>
-            <li>{""}</li>
-          </ul>
-        </div>
       </div>
     </div>
   );
